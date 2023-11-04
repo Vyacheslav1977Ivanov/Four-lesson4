84322dac189abedc6f1ccc94a1163296ed816b76
Vyacheslav Ivanov <vya1977@mail.ru>
2023-11-04 14:28:47 +0300

    Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,20 @@
+﻿int Promt(string message)           
+{
+    Console.Write(message);
+    string readInput = Console.ReadLine();
+    int result = int.Parse(readInput);
+    return result;
+}
+
+int SumAllNumbers(int number)
+{
+    int result = 0;
+    while (number > 0)
+    {                                                                                                                                           // 3
+        result+= number % 10; 
+        number = number / 10;   
+    }
+    return result;
+}
+int number = Promt("Введи число: ");
+Console.WriteLine($"Сумма всех чисел в цифре {number} = {SumAllNumbers(number)}");
