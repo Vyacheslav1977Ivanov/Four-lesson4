85020a99529c871c1bde259a27db7f1507b1d158
Vyacheslav Ivanov <vya1977@mail.ru>
2023-11-04 14:31:43 +0300

    Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,22 @@
+﻿int[] GenerateArray()
+{
+    int[] array = new int[8];     
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(1, 100);
+    }
+
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write($"Сгенерированный массив размерностью {array.Length} элементов : ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+}
+
+int [] result = GenerateArray(); 
+PrintArray(result);
