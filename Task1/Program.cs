e684d3174391d794dbeb0157634fb46c605aaa0e
Vyacheslav Ivanov <vya1977@mail.ru>
2023-11-04 14:25:08 +0300

    Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,18 @@
+﻿void numberAB(double a , double b)
+{
+    
+    while(a < 10 && b < 10)
+    {
+        double result = Math.Pow(a,b);
+        Console.WriteLine($" {a} ^ {b} = {result}");
+        a++;
+        b++;
+    }
+  
+}
+
+Console.Write("Введите число A : ");
+double x = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите число B : ");
+double y = Convert.ToDouble(Console.ReadLine());
+numberAB(x,y);
